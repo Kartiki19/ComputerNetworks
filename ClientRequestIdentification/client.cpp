03a664a6c74829f8e336997569df99604d4b2905
kartiki19 <kartikid1911@gmail.com>
2023-06-02 14:09:51 -0700

    Added folder

---
+++ ClientRequestIdentification/client.cpp
@@ -0,0 +1,113 @@
+//#include <iostream>
+#include "clientServer.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <sys/time.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+
+using namespace std;
+
+PacketFormat createPacket(){
+    PacketFormat packet;
+    packet.startOfPacketId = 0xFFFF;
+    packet.clientId = 0xFF;
+    packet.endOfPacketId = 0xFFFF;
+    return packet;
+}
+
+int main(){
+
+    /// Create UDP Socket
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if(sock < 0){
+        cerr << "Failed to create socket" << endl;
+        return 1;
+    }
+
+    /// Assign IP and Port for Server
+    sockaddr_in serverAddress;
+    socklen_t serverAddressSize;
+
+	bzero(&serverAddress, sizeof(serverAddress));
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+	serverAddress.sin_port=htons(PORT);
+	serverAddressSize = sizeof(serverAddress);
+
+    /// Setting ACK Timer of 3 sec
+    timeval timer;
+    timer.tv_sec = TIMER;
+    timer.tv_usec = 0;
+
+    /// Setting options for socket to wait for 3 sec for receiving data
+    if (setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &timer, sizeof(timer)) == -1) {
+        cerr << "Failed to set receive timeout : " << strerror(errno) << endl;
+        close(sock);
+        return 1;
+    }
+
+    /// Creating the Packet
+    PacketFormat requestPacket = createPacket();
+
+    /// Reading Input
+    FILE *inputFile = fopen("input.txt", "rt");
+    if(inputFile == NULL) {
+        cerr << "Failed to Open the File input.txt" << endl;
+        return 1;
+    }
+
+    char line[30]; /// Characters in 1 line of input file ( Subscriber Number + technology)
+    int segmentNum = 1; /// To keep track of segments
+    while(fgets(line, sizeof(line), inputFile)){
+        cout << "\n******************* New Request : "<< segmentNum <<"*******************\n";
+        char* sourceSubscriberNum = strtok(line," ");
+        requestPacket.length = strlen(sourceSubscriberNum);
+        requestPacket.sourceSubscriberNum = atoi(sourceSubscriberNum);
+
+        char* technology = strtok(NULL," "); /// NULL for continuing the same string
+        requestPacket.length += strlen(technology);
+        requestPacket.technology = atoi(technology);
+        
+        requestPacket.segmentNumber = segmentNum;
+        printPacket(requestPacket);
+
+        int iteration = 0;
+        while(iteration < MAX_ITERATIONS){
+            /// Sending Request Packet to the Server
+            sendto(sock, &requestPacket,sizeof(PacketFormat), 0, (struct sockaddr *)&serverAddress, serverAddressSize);
+            iteration++;
+
+			/// Receiving the response from server, receivedBytes will be less than 0, if there is a timeout (3 sec timer)
+            PacketFormat responsePacket;
+			int received = recvfrom(sock, &responsePacket, sizeof(PacketFormat), 0, NULL, NULL);
+			if(received <= 0){
+                cout << "\n********* ACK not received *********" << endl;
+                //sentSuccessfully = false;
+            }
+            else if(responsePacket.type == Status::NOT_PAID){
+                cout << "Subscriber has not paid !" << endl;
+            }
+            else if (responsePacket.type == Status::NOT_EXIST){
+                cout << "Subscriber does not exist !" << endl;
+            }
+            else if (responsePacket.type == Status::ACCESS_OK){
+                cout << "Subscriber has paid, permitted access to the network !" << endl;
+            }
+        }
+
+        if(iteration >= MAX_ITERATIONS){ /// After re-sending the packet 3 times, the ACK is not received
+            cout << "\n************** SERVER DID NOT RESPOND **************" << endl;
+            exit(0);
+        }
+
+        segmentNum++;
+    }
+
+    fclose(inputFile);
+    return 0;
+}
---
+++ ClientRequestIdentification/clientServer.h
@@ -0,0 +1,46 @@
+
+#ifndef CLIENTSERVER_H
+#define CLIENTSERVER_H
+#include <cstdint> 
+#include <iostream>
+/// Protocol Premitives
+#define START_PACKET_ID 0xFFFF
+#define END_PACKET_ID 0xFFFF
+#define CLIENT_ID 0xFF
+
+#define LENGTH_MAX 0xFF
+#define TIMER 3
+#define MAX_ITERATIONS 3
+#define PORT 8080
+
+/// Identification Types
+enum Status {
+    ACCESS_OK = 0xFFFB,
+    NOT_PAID = 0xFFF9,
+    NOT_EXIST = 0xFFFA
+};
+
+/// Structure of a Packet
+struct PacketFormat {
+	uint16_t startOfPacketId;
+	uint8_t clientId;
+	uint16_t type;
+	uint8_t segmentNumber;
+	uint8_t length;
+	uint8_t technology;
+	unsigned int sourceSubscriberNum;
+	uint16_t endOfPacketId;
+};
+
+void printPacket(PacketFormat packet){
+    std::cout << "Start ID: " << packet.startOfPacketId << std::endl;
+    std::cout << "Client ID: " << static_cast<int>(packet.clientId) << std::endl;
+    std::cout << "Type: " << static_cast<Status>(packet.type) << std::endl;
+    std::cout << "Segment Number: " << static_cast<int>(packet.segmentNumber) << std::endl;
+    std::cout << "Length: " << static_cast<int>(packet.length) << std::endl;
+    std::cout << "Technology: " << static_cast<int>(packet.technology) << std::endl;
+    std::cout << "Source Subscriber No.: " << packet.sourceSubscriberNum << std::endl;
+    std::cout << "End ID: " << packet.endOfPacketId << std::endl;
+}
+
+#endif
